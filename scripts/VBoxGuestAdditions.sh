ae9bd5f96913bb3f229fb57bb67656abee839534
Adam Goldsmith <adam.goldsmith75@gmail.com>
2017-06-25 20:21:25 +0100

    Updating comments in cinnamon18 script

--- scripts/VBoxGuestAdditions.sh
+++ scripts/VBoxGuestAdditions.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+# Reluctantly put these steps in this script to overcome VirtualBox's GuestAddition
+# installation process returning RC 2 even when successful
+# Annoyingly, packer doesn't have an option to ignore errors for known situations like this
 
 mount -ro loop ~/VBoxGuestAdditions.iso /mnt
-/mnt/VBoxLinuxAdditions.run -- --force
+/mnt/VBoxLinuxAdditions.run -- --force		# --force needed to overwrite existing GuestAdditions without being prompted
 umount /mnt
 
 exit 0
